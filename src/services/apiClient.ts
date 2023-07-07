065d198e286608cfb97651437fd848c3c6460c9c
Xero1000 <alex.heil@comcast.net>
2023-07-07 11:35:08 -0400

    Created custom hook to fetch the games

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
@@ -6,3 +6,5 @@ export default axios.create({
         key: "5a00279a0cb347bcae546ecf4525c8a4"
     }
 })
+
+export { CanceledError }
