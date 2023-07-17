3fec38195d532c5583efc85f371711fc4bf38a67
Xero1000 <alex.heil@comcast.net>
2023-07-17 10:17:46 -0400

    Refactoring - Fetch games with React Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query"
 import { GameQuery } from "../App"
-import useData from "./useData"
+import { FetchResponse } from "../services/apiClient"
+import apiClient from "../services/apiClient"
 
 export interface Platform {
     id: number
@@ -16,28 +18,19 @@ export interface Game {
     rating_top: number
 }
 
-const useGames = (gameQuery: GameQuery) => 
-    useData<Game>(
-        "/games", 
-        { 
-            params: {
-                genres: gameQuery.genre?.id, 
-                parent_platforms: gameQuery.platform?.id,
-                ordering: gameQuery.sortOrder,
-                search: gameQuery.searchText
-            }
-        }, 
-        [gameQuery]
-    )
+const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: () => 
+        apiClient
+            .get<FetchResponse<Game>>("/games", {
+                params: {
+                    genres: gameQuery.genre?.id, 
+                    parent_platforms: gameQuery.platform?.id,
+                    ordering: gameQuery.sortOrder,
+                    search: gameQuery.searchText
+                }
+            })
+            .then(res => res.data)
+})
 
 export default useGames;
-
-// const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => 
-//     useData<Game>("/games", { 
-//         params: {
-//             genres: selectedGenre?.id, platforms: selectedPlatform?.id}
-//         }, 
-//         [selectedGenre?.id, selectedPlatform?.id]
-//     )
-
-// export default useGames;
