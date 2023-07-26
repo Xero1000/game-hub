b0b173b386a1a9be0f1cb80be4d7b2dd9ab6cd7e
Xero1000 <alex.heil@comcast.net>
2023-07-26 10:51:44 -0400

    Refactor- manage state with Zustand

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,27 +1,49 @@
-import useGenres, { Genre } from "../hooks/useGenres";
-import { List, ListItem, HStack, Image, Spinner, Button, Heading } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+} from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/imageUrl";
+import useGameQueryStore from "../store";
 
-interface Props {
-    onSelectGenre: (genre: Genre) => void
-    selectedGenreId?: number;
-}
-
-const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
+const GenreList = () => {
   const { data, isLoading, error } = useGenres();
+  const selectedGenreId = useGameQueryStore((s) => s.gameQuery.genreId);
+  const setSelectedGenreId = useGameQueryStore((s) => s.setGenreId);
 
-  if (error) return null
-  if (isLoading) return <Spinner />
+  if (error) return null;
+  if (isLoading) return <Spinner />;
 
   return (
     <>
-      <Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
       <List>
         {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
-              <Image boxSize="32px" borderRadius={8} objectFit="cover" src={getCroppedImageUrl(genre.image_background)} />
-              <Button whiteSpace="normal" textAlign="left" fontWeight={genre.id === selectedGenreId ? "bold" : "normal"} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={genre.id === selectedGenreId ? "bold" : "normal"}
+                onClick={() => setSelectedGenreId(genre.id)}
+                fontSize="lg"
+                variant="link"
+              >
+                {genre.name}
+              </Button>
             </HStack>
           </ListItem>
         ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query"
 import ms from "ms"
-import { GameQuery } from "../App"
 import APIClient, { FetchResponse } from "../services/apiClient"
 import { Platform } from "./usePlatforms"
+import useGameQueryStore from "../store"
 
 const apiClient = new APIClient<Game>("/games")
 
@@ -15,23 +15,27 @@ export interface Game {
     rating_top: number
 }
 
-const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>, Error>({
-    queryKey: ["games", gameQuery],
-    queryFn: ({ pageParam = 1 }) => 
-        apiClient
-            .getAll({
-                params: {
-                    page: pageParam,
-                    genres: gameQuery.genreId, 
-                    parent_platforms: gameQuery.platformId,
-                    ordering: gameQuery.sortOrder,
-                    search: gameQuery.searchText
-                }
-            }),
-        getNextPageParam: (lastPage, allPages) => {
-            return lastPage.next ? allPages.length + 1 : undefined
-        },
-        staleTime: ms("24h")
-})
+const useGames = () => {
+    const gameQuery = useGameQueryStore(s => s.gameQuery)
+
+    return useInfiniteQuery<FetchResponse<Game>, Error>({
+        queryKey: ["games", gameQuery],
+        queryFn: ({ pageParam = 1 }) => 
+            apiClient
+                .getAll({
+                    params: {
+                        page: pageParam,
+                        genres: gameQuery.genreId, 
+                        parent_platforms: gameQuery.platformId,
+                        ordering: gameQuery.sortOrder,
+                        search: gameQuery.searchText
+                    }
+                }),
+            getNextPageParam: (lastPage, allPages) => {
+                return lastPage.next ? allPages.length + 1 : undefined
+            },
+            staleTime: ms("24h")
+    })
+}
 
 export default useGames;
