f9b667ac69b4ee8ec2489a91aa392d14daca5beb
Xero1000 <alex.heil@comcast.net>
2023-08-01 09:40:34 -0400

    Refactor- Move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,32 +1,34 @@
-import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react"
-import { Game } from '../hooks/useGames'
-import PlatformIconList from "./PlatformIconList"
-import CriticScore from "./CriticScore"
-import getCroppedImageUrl from "../services/imageUrl"
-import Emoji from "./Emoji"
-import { color } from "framer-motion"
-import { Link } from "react-router-dom"
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+import { Game } from "../entities/Game";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/imageUrl";
+import Emoji from "./Emoji";
+import { color } from "framer-motion";
+import { Link } from "react-router-dom";
 
 interface Props {
-    game: Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-        <Image src={getCroppedImageUrl(game.background_image)}/>
-        <CardBody>
-            <HStack justifyContent="space-between" marginBottom={3}>
-                <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
-                <CriticScore score={game.metacritic} /> 
-            </HStack>
-            <Heading fontSize="2xl">
-              <Link to={"/games/" + game.slug}>{game.name}</Link>
-              <Emoji rating={game.rating_top} />
-            </Heading>
-        </CardBody>
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <Heading fontSize="2xl">
+          <Link to={"/games/" + game.slug}>{game.name}</Link>
+          <Emoji rating={game.rating_top} />
+        </Heading>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
